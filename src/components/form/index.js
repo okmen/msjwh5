52744630f957f48d0de086f6f22a71ccbde0f79a
王观伟 <wangguanwei@chudaokeji.com>
2017-12-04 16:37:54 +0800

    no message

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,8 +1,12 @@
 import GInput from './GInput'
 import GSelect from './GSelect'
 import GButton from './GButton'
+import GUpload from './GUpload'
+import Group from './Group'
 export {
   GInput,
   GSelect,
-  GButton
+  GButton,
+  GUpload,
+  Group
 }
--- src/views/transactionClass/drivingLicence/annualExaminations.vue
+++ src/views/transactionClass/drivingLicence/annualExaminations.vue
@@ -4,18 +4,28 @@
   <g-input title="身份证号码" v-model="IDCard"></g-input>
   <g-input title="手机号码" v-model="mobilePhone"></g-input>
   <g-select title="户籍所在地" :data="censusRegisterList" v-model="censusRegisterOne"></g-select>
-  <g-button text="确认信息" @click.native="confirmInfo"></g-button>
+  <group title="请按示例图上传以下证件照片">
+    <div class="upload-group">
+      <g-upload text="身份证（正面)" id="file1" :bg="require('../../../assets/images/IDcard-front.png')" v-model="IDCardFront"></g-upload>
+      <g-upload text="身份证（反面)" id="file2" :bg="require('../../../assets/images/IDcard-back.png')" v-model="IDCardBack"></g-upload>
+      <g-upload text="审核教育绘制表" id="file3" :bg="require('../../../assets/images/edu-table.png')" v-model="eduTable"></g-upload>
+    </div>
+  </group>
+ <g-button text="确认信息" @click.native="confirmInfo"></g-button>
 </div>
 </template>
 
 <script>
-  import {GInput, GSelect, GButton} from 'form'
+  import {GInput, GSelect, GButton, GUpload, Group} from 'form'
   export default {
     data () {
       return {
         userName: '',
         IDCard: '',
         mobilePhone: '',
+        IDCardFront: '',
+        IDCardBack: '',
+        eduTable: '',
         censusRegisterList: [
           {name: '深圳', value: 0}, {name: '港澳台籍', value: 1}, {name: '外国籍', value: 2}, {name: '其他', value: 3}
         ],
@@ -25,16 +35,22 @@
     components: {
       GInput,
       GSelect,
-      GButton
+      GButton,
+      GUpload,
+      Group
     },
     methods: {
       confirmInfo () {
-        console.log(this.censusRegisterOne)
+        console.log(this.IDCardFront)
       }
     }
   }
 </script>
 
 <style scoped>
-
+.upload-group{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
