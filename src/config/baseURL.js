a19ee6f8941d93d71d074e4feca74c83c58e4a98
liwang <liwang@chudaokeji.com>
2017-12-04 19:45:08 +0800

    提交

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -14,3 +14,7 @@ export const getUserM = `${commonUrl}convenience/getMSJWinfo.html` // 民生警�
 export const userAgreement = `${commonUrl}user/getDocumentationORMByNoticeKey.html` // 用户须知接口
 
 export const login = `${commonUrl}user/login.html` // 登录接口
+
+export const cardRepair = `${commonUrl}handleservice/repairDriverLicense.html` // 驾驶证补证
+
+export const cardReplace = `${commonUrl}handleservice/replaceDriverLicense.html` // 驾驶证换证
