44800789c79033cb06f0f4245d97766a6be689ed
linzicong <linzicong@chudaokeji.com>
2017-12-05 17:56:01 +0800

    新增补换号牌路由

--- src/router/transactionClass/motorVehicles.js
+++ src/router/transactionClass/motorVehicles.js
@@ -5,6 +5,14 @@ export default [
     meta: {
       title: '申请类'
     },
-    component: () => import('@/views//transactionClass/motorVehicles/applyClass/applyClass.vue')
+    component: () => import('@/views/transactionClass/motorVehicles/applyClass/applyClass.vue')
+  },
+  {
+    name: 'replaceLicencePlate',
+    path: 'replaceLicencePlate',
+    meta: {
+      title: '补领机动车号牌'
+    },
+    component: () => import('@/views/transactionClass/motorVehicles/replacementClass/replaceLicencePlate.vue')
   }
 ]
