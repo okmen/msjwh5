4cc6ae396bd963ee93fccdd8e4c40576d073ba19
liwang <liwang@chudaokeji.com>
2017-12-11 17:48:46 +0800

    提交

--- src/views/transactionClass/dealServiceMsjw.vue
+++ src/views/transactionClass/dealServiceMsjw.vue
@@ -2,10 +2,10 @@
   <!-- 驾驶证类-驾驶证业务导航页 -->
   <div class="carService-outer">
     <div class="query-link" :class="{ 'subscribeService': source === 'M'}">
-      <router-link :to="{ path: '/userAgreement/jszbzhz', query: queryURL }">驾驶证补换证</router-link>
+      <router-link :to="{ path: '/userAgreement/jszns', query: queryURL }">驾驶证年审</router-link>
     </div>
     <div class="query-link" :class="{ 'subscribeService': source === 'M'}">
-      <router-link :to="{ path: '/userAgreement/jszns', query: queryURL }">驾驶证年审</router-link>
+      <router-link :to="{ path: '/userAgreement/jszbzhz', query: queryURL }">驾驶证补换证</router-link>
     </div>
   </div>
 </template>
--- src/views/transactionClass/transactionService.vue
+++ src/views/transactionClass/transactionService.vue
@@ -2,22 +2,16 @@
   <!-- 办理类-驾驶证业务导航页 -->
   <div class="carService-outer">
     <div class="query-link" :class="{ 'subscribeService': source === 'M'}">
-      <router-link :to="{ path: '/userAgreement/jszbzhz', query: queryURL }">驾驶证补换证</router-link>
-    </div>
-    <div class="query-link" :class="{ 'subscribeService': source === 'M'}">
-      <router-link :to="{ path: '/userAgreement/jszns', query: queryURL }">驾驶证年审</router-link>
-    </div>
-    <div class="query-link" :class="{ 'subscribeService': source === 'M'}">
       <router-link :to="{ path: '/applyInterimCard', query: queryURL }">申请机动车临牌</router-link>
     </div>
     <div class="query-link" :class="{ 'subscribeService': source === 'M'}">
-      <router-link :to="{ path: '/userAgreement/szjj_hander_rmvp', query: queryURL }">补领机动车号牌</router-link>
+      <router-link :to="{ path: '/userAgreement/szjj_hander_id', query: queryURL }">机动车委托异地定期检验申报</router-link>
     </div>
     <div class="query-link" :class="{ 'subscribeService': source === 'M'}">
       <router-link :to="{ path: '/userAgreement/szjj_hander_rim', query: queryURL }">补换检验合格标志</router-link>
     </div>
     <div class="query-link" :class="{ 'subscribeService': source === 'M'}">
-      <router-link :to="{ path: '/userAgreement/szjj_hander_id', query: queryURL }">机动车委托异地定期检验申报</router-link>
+      <router-link :to="{ path: '/userAgreement/szjj_hander_rmvp', query: queryURL }">补领机动车号牌</router-link>
     </div>
   </div>
 </template>
